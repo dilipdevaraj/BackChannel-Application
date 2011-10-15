15e50da7d1730854fa53c520928822ab11b33da4
Dilip Devaraj <ddilipd@ncsu.edu>
2011-10-15 14:36:28 -0300

    Update app/controllers/posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.find_all_by_postId(nil)
+    @sortedpost = sortposts
     @title = 'Back Channel App - Home'
     respond_to do |format|
       format.html # index.html.erb
@@ -156,6 +157,115 @@ class PostsController < ApplicationController
         end
       end
     end
+  end
+
+  def sortposts
+    # sorting the posts according to the weights
+    sortedpost = Post.find_all_by_postId(nil)
+    sortedpost.each do |post|
+      vote_post = Vote.find_all_by_post_id(post.id)
+      vote_count = vote_post.count
+      time_difference_sec = Time.now.utc - post.created_at
+      minutes  = time_difference_sec / 60
+      hours = (minutes / 60).to_int
+      days  = (hours / 24).to_int
+      months = (days/30).to_int
+      years =  (months/12).to_int
+      score = 0
+
+      if years > 0
+        score = 0
+      elsif months > 0
+        if months < 3
+          score = 4
+        elsif months < 6
+          score = 3
+        elsif months < 9
+          score = 2
+        elsif months < 12
+          score = 1
+        end
+      elsif days > 0
+        if days < 5
+          score = 10
+        elsif days < 10
+          score = 9
+        elsif days < 15
+          score = 8
+        elsif days < 20
+          score = 7
+        elsif days < 25
+          score = 6
+        elsif days < 31
+          score = 5
+        end
+      elsif hours > 0
+        if hours < 4
+          score = 16
+        elsif hours < 8
+          score = 15
+        elsif hours < 12
+          score = 14
+        elsif hours < 16
+          score = 13
+        elsif hours < 20
+          score = 12
+        elsif hours < 24
+          score = 11
+        end
+      elsif minutes > 0
+        if minutes < 1
+          score = 36
+        elsif minutes < 2
+          score = 35
+        elsif minutes < 3
+          score = 34
+        elsif minutes < 4
+          score = 33
+        elsif minutes < 5
+          score = 32
+        elsif minutes < 6
+          score = 31
+        elsif minutes < 7
+          score = 30
+        elsif minutes < 8
+          score = 29
+        elsif minutes < 9
+          score = 28
+        elsif minutes < 10
+          score = 27
+        elsif minutes < 15
+          score = 26
+        elsif minutes < 20
+          score = 25
+        elsif minutes < 25
+          score = 24
+        elsif minutes < 30
+          score = 23
+        elsif minutes < 35
+          score = 22
+        elsif minutes < 40
+          score = 21
+        elsif minutes < 45
+          score = 20
+        elsif minutes < 50
+          score = 19
+        elsif minutes < 55
+          score = 18
+        elsif minutes < 60
+          score = 17
+        end
+      else
+        score = 37
+
+
+      end
+      post.weight = vote_count * 60 + 40 * score
+      post.save
+
+    end
+
+    return sortedpost
   end
 
 end
